b2d67c634d38dce716fedda4677c830582bf8bd7
Andrea Giardina <andrea@hoverstate.com>
2017-11-04 08:51:35 +0100

    Added support to select commit on tavleview

--- src/cdflow/controller.clj
+++ src/cdflow/controller.clj
@@ -2,9 +2,9 @@
   (:require [clojure.java.io :as io]
             [cdflow.git :as git]
             [cdflow.state :as state]
-            [clojure.pprint :as pp]
-            )
-  (:import [javafx.event ActionEvent]
+            [clojure.pprint :as pp])
+  (:import [javafx.event ActionEvent EventHandler]
+           [javafx.scene.input MouseEvent]
            [javafx.stage Stage DirectoryChooser FileChooser StageStyle Window Modality]
            [javafx.application Platform]
            [javafx.scene Scene]
@@ -14,12 +14,15 @@
            (javafx.collections ObservableList)
            (javafx.collections FXCollections)
            (javafx.scene.control TableColumn)
+           (javafx.scene.control TableRow)
+           (javafx.util Callback)
            [java.awt Desktop])
 
   (:gen-class
-    :methods [[onLoad [javafx.event.ActionEvent] void]
-              [onOpen [javafx.event.ActionEvent] void]]
-    ))
+    :methods
+    [[onLoad [javafx.event.ActionEvent] void]
+     [onOpen [javafx.event.ActionEvent] void]
+     [onSelectCommit [javafx.scene.input.MouseEvent] void]]))
 
 (def current-stage (atom nil))
 
@@ -32,44 +35,44 @@
 
 (defn create-menu [node parent]
   (let [current-node (create-item (first node) parent)
-        children (rest node)]
+        children     (rest node)]
     (doall (map #(create-menu % current-node) children))
     current-node))
 
 ;@todo manage error for
 (defn showBranches [tree repo opt]
-    (.setRoot tree (create-menu (git/branch-tree (.getAbsolutePath repo) opt) nil)))
+  (.setRoot tree (create-menu (git/branch-tree (.getAbsolutePath repo) opt) nil)))
 
 (defn show-commits [scene repo]
   (let [description (.lookup scene "#commitstableDescription")
 
-        commit   (doto (TableColumn. "Commit") (.setMinWidth 100))
-        date  (doto (TableColumn. "Date") (.setMinWidth 150))
+        commit      (doto (TableColumn. "Commit") (.setMinWidth 100))
+        date        (doto (TableColumn. "Date") (.setMinWidth 150))
         description (doto (TableColumn. "Description") (.setMinWidth 70))
 
-        table (.lookup scene "#commitstable")
-        commits (FXCollections/observableArrayList (git/get-all-ref-commits repo))
-        ]
+        table       (.lookup scene "#commitstable")
+        commits     (FXCollections/observableArrayList (git/get-all-ref-commits repo))]
 
 
-  (doto table
-  (-> .getColumns (.addAll [commit date description]))
-        (.setItems commits))
+    (doto table
+          (-> .getColumns (.addAll [commit date description]))
+          (.setItems commits))
 
-      (.. description (setCellValueFactory (PropertyValueFactory. "fullMessage")))
-      (.. commit (setCellValueFactory (PropertyValueFactory. "name")))
-      (.. date (setCellValueFactory (PropertyValueFactory. "commitTime")))
+    (.. description (setCellValueFactory (PropertyValueFactory. "fullMessage")))
+    (.. commit (setCellValueFactory (PropertyValueFactory. "name")))
+    (.. date (setCellValueFactory (PropertyValueFactory. "commitTime")))))
 
-    ))
+(defn -onSelectCommit [this ^MouseEvent event]
+  (clojure.pprint/pprint (.getName (.getSelectedItem (.getSelectionModel (.getSource event))))))
 
 (defn -onLoad [this ^ActionEvent event]
   (if (not (nil? (state/get-repository)))
-    (let [source (.getSource event)
-          scene (.getScene source)
-          editor (.lookup scene "#editor")
+    (let [source  (.getSource event)
+          scene   (.getScene source)
+          editor  (.lookup scene "#editor")
           webview (.lookup scene "#webview")
           menuBar (.lookup scene "#menuBar")
-          engine (.getEngine webview)]
+          engine  (.getEngine webview)]
 
       (.set (.useSystemMenuBarProperty menuBar) true)
       (.load engine (.toString (io/resource "tree/index.html")))
@@ -78,17 +81,19 @@
 (defn -onOpen [this ^ActionEvent event]
   (let [chooser (doto (DirectoryChooser.)
                       (.setTitle "Import"))
-        repo (.showDialog chooser (Stage.))
-        scene (->> event
-                   .getSource
-                   .getParentPopup
-                   .getOwnerWindow
-                   .getScene)
+        repo    (.showDialog chooser (Stage.))
+        scene   (->> event
+                     .getSource
+                     .getParentPopup
+                     .getOwnerWindow
+                     .getScene)
         webview (.lookup scene "#webview")
-        engine (.getEngine webview)]
+        engine  (.getEngine webview)]
     (state/set-repository (.getAbsolutePath repo))
     (showBranches (.lookup scene "#branches") repo :local)
     (showBranches (.lookup scene "#branchesorigin") repo :remote)
     (show-commits scene repo)
+
+    ;    (clojure.pprint/pprint (git/branch-list-contains (.getAbsolutePath repo) "b09009c1a4000cb5d3ab62255b3a6eaf5a967b0e"))
 
     (.load engine (.toString (io/resource "tree/index.html")))))
